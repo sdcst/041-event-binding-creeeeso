406fa7f17d097d037bfe7173055c11556be2cbb0
creeeeso <crystalf7319@gmail.com>
2024-05-30 13:28:46 -0700

    2

---
+++ task1.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+import playsound as p
+
+def playsound(event):
+    print(event)
+    p.playsound("animals_dogs_x2_barking_small_001.mp3")
+
+win = tk.Tk()
+
+b1 =  tk.Button(win,text="Click to play",command=playsound)
+
+b1.pack()
+
+win.mainloop()
