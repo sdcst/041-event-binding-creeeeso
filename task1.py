4a60ac5748ef43818e173476532397ee2f01e35b
creeeeso <crystalf7319@gmail.com>
2024-06-04 14:19:07 -0700

    6

--- task1.py
+++ task1.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 import playsound as p
+from tkinter import *
+from tkinter import ttk
 
 def BOOM(event):
     print(event)
@@ -22,19 +24,26 @@ def suspense(event):
     p.playsound("audio/SUSPENSE_SOUND_EFFECTS.mp3")
 
 win = tk.Tk()
+win.title("Sound Board")
 
-b1 = tk.Button(win,text="Click to play")
-b2 = tk.Button(win,text="Click to play")
-b3 = tk.Button(win,text="Click to play")
-b4 = tk.Button(win,text="Click to play")
-b5 = tk.Button(win,text="Click to play")
+vineboomimg = PhotoImage(file = "vineboom.png",width=250, height=250)
+bounceimg = PhotoImage(file = "bounce.png",width=250, height=250)
+amongusimg = PhotoImage(file = "amongus.png",width=250, height=250)
+minecraftimg = PhotoImage(file = "minecraft.png",width=250, height=250)
+suspenseimg = PhotoImage(file = "suspense.png",width=250, height=250)
+bounceimg = PhotoImage(file = "bounce.png",width=250, height=250)
 
-l1 = tk.Label(win, text="Sound Board")
+b1 = tk.Button(win,text="Click to play", image=vineboomimg)
+b2 = tk.Button(win,text="Click to play", image=bounceimg)
+b3 = tk.Button(win,text="Click to play", image=amongusimg)
+b4 = tk.Button(win,text="Click to play", image=minecraftimg)
+b5 = tk.Button(win,text="Click to play", image=suspenseimg)
+
+l1 = tk.Label(win, text="Vine Boom")
 l2 = tk.Label(win, text="Bounce")
 l3 = tk.Label(win, text="Among Us Effect")
 l4 = tk.Label(win, text="Minecraft Eating")
 l5 = tk.Label(win, text="Suspense")
-l6 = tk.Label(win, text="Sound Board")
 
 b1.bind("<Button>",BOOM)
 b2.bind("<Button>",bounce)
@@ -42,15 +51,16 @@ b3.bind("<Button>",AmongUs)
 b4.bind("<Button>",minecraft)
 b5.bind("<Button>",suspense)
 
-l1.grid(row=1, column=1)
-b1.grid(row=1, column=2)
-l2.grid(row=2, column=1)
-b2.grid(row=2, column=2)
-l3.grid(row=3, column=1)
-b3.grid(row=3, column=2)
+l1.grid(row=2, column=1)
+b1.grid(row=1, column=1)
+l2.grid(row=2, column=2)
+b2.grid(row=1, column=2)
+l3.grid(row=2, column=3)
+b3.grid(row=1, column=3)
 l4.grid(row=4, column=1)
-b4.grid(row=4, column=2)
-l5.grid(row=5, column=1)
-b5.grid(row=5, column=2)
+b4.grid(row=3, column=1)
+l5.grid(row=4, column=2)
+b5.grid(row=3, column=2)
+
 
 win.mainloop()
